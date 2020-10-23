c7cda2157f63d611bdade232c5f67b09e9b9aa13
Unilfo <kokopal13@gmail.com>
2020-10-23 14:39:35 +0300

    Updates

---
+++ precache-manifest.cc9a2186c92550823e86a125ab168fcf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "364f4bbcb9167d43b3c9b22dd09dcfac",
+    "url": "/tpsite_modules/index.html"
+  },
+  {
+    "revision": "dce7fd73635fc1923155",
+    "url": "/tpsite_modules/static/css/2.156c132a.chunk.css"
+  },
+  {
+    "revision": "2430e0993423054f9dbe",
+    "url": "/tpsite_modules/static/css/main.41d92ca1.chunk.css"
+  },
+  {
+    "revision": "dce7fd73635fc1923155",
+    "url": "/tpsite_modules/static/js/2.f94001f6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/tpsite_modules/static/js/2.f94001f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2430e0993423054f9dbe",
+    "url": "/tpsite_modules/static/js/main.7fdd7070.chunk.js"
+  },
+  {
+    "revision": "fc326a2a8620f9ca031a",
+    "url": "/tpsite_modules/static/js/runtime-main.711857a9.js"
+  }
+]);
